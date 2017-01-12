2c6e673f628cb7ff0610c9772f7dc34301b2ae56
linuxguy467 <dpplrdr435@gmail.com>
2017-01-12 09:36:26 -0500

    Added main folders and files

---
+++ src/main/java/com/mongodb/HelloWorldSparkStyle.java
@@ -0,0 +1,21 @@
+package com.mongodb;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+/**
+ * Created by mchem on 1/11/2017.
+ */
+public class HelloWorldSparkStyle {
+    public static void main(String[] args) {
+        Spark.get(new Route("/") {
+            @Override
+            public Object handle(final Request request,
+                                 final Response response) {
+                return "Hello World from Spark!";
+            }
+        });
+    }
+}
